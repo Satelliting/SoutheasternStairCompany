2baec74e0568f1f8eea1a123b3e7229fc6192aba
Satelliting <jordanallen1332@gmail.com>
2024-11-01 22:17:41 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+<div style="text-align: center;">
+  <img src="app/assets/img/logo.svg" alt="Logo" style="width: 300px;" />
+  <h1>Southeastern Stair Company</h1>
+  <h2>You're one stop solution for quality stairs, installation & finishing!</h2>
+</div>
+
+Are you in search of a stair company that excels in quality craftsmanship, precise installations, and expert staining and painting? Look no further! At Southeastern Stair Company, we are committed to delivering the finest staircases in the Southeast. Our expertise lies in custom staircase design, exceptional craftsmanship, and comprehensive stair solutions tailored for both residential and commercial spaces. Let us transform your vision into reality!
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ app/assets/js/main.js
@@ -0,0 +1,33 @@
+document.addEventListener("wheel", function (event) {
+  event.preventDefault();
+
+  const h1Elements = document.querySelectorAll("h1");
+  const scrollDirection = event.deltaY > 0 ? "down" : "up";
+
+  let currentH1Index = Array.from(h1Elements).findIndex((h1) => {
+    const rect = h1.getBoundingClientRect();
+    return rect.top >= 0 && rect.bottom <= window.innerHeight;
+  });
+
+  if (scrollDirection === "down") {
+    if (currentH1Index < h1Elements.length - 1) {
+      currentH1Index++;
+    }
+  } else {
+    if (currentH1Index > 0) {
+      currentH1Index--;
+    }
+  }
+
+  if (currentH1Index >= 0) {
+    const targetH1 = h1Elements[currentH1Index];
+    const offset = 120; // Height of the padding
+    const targetPosition =
+      targetH1.getBoundingClientRect().top + window.scrollY - offset; // Adjust position
+
+    window.scrollTo({
+      top: targetPosition,
+      behavior: "smooth",
+    });
+  }
+});
